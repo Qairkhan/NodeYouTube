b3d034dd06d94f5c15f55760c1a428e321dc2386
qairkhan <shyngys94@gmail.com>
2020-06-01 17:57:42 +0300

    path dirname fix

--- demo/path.js
+++ demo/path.js
@@ -1,4 +1,4 @@
 const path = require('path')
 
 console.log('file name: ', path.basename(__filename))
-console.log('dir name: ', path.dirname(__dirname))
+console.log('dir name: ', path.dirname(__filename))
