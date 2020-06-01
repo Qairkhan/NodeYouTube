839a9a7b1cad8c7454ae47282f78212fcf3e2387
qairkhan <shyngys94@gmail.com>
2020-06-01 17:58:25 +0300

    path extname

--- demo/path.js
+++ demo/path.js
@@ -2,3 +2,4 @@ const path = require('path')
 
 console.log('file name: ', path.basename(__filename))
 console.log('dir name: ', path.dirname(__filename))
+console.log('extname name: ', path.extname(__filename))
