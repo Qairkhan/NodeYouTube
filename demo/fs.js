0001146e2a83c05e3e69ca484bd4949d97ade34e
Qairkhan <shyngys94@gmail.com>
2020-06-02 09:27:07 +0300

    append file nodeJS

--- demo/fs.js
+++ demo/fs.js
@@ -8,9 +8,16 @@ fs.mkdir(path.join(__dirname, "test"), (err)=>{
 })
 
 const filePath = path.join (__dirname, 'text', 'text.txt')
-fs.writeFile(filePath, "Hello nodeJS", (e)=> {
-    if (e){
-        throw e
+fs.writeFile(filePath, "Hello nodeJS", (err)=> {
+    if (err){
+        throw err
+    }
+    console.log("file created")
+})
+
+fs.appendFile(filePath, "\nHello nodeJS again", (err)=> {
+    if (err){
+        throw err
     }
     console.log("file created")
 })
