bd71b7190f39b966222cdba1e1e3ad6d54a9920e
qairkhan <shyngys94@gmail.com>
2020-06-04 16:40:51 +0300

    path

--- index.js
+++ index.js
@@ -3,8 +3,28 @@ const fs = require('fs')
 const path = require('path')
 
 const server = http.createServer((req, res)=> {
-    res.end('<h1>hello NodeJS server</h1>')
-})
+    if (req.url === "/") {
+        fs.readFile(path.join(__dirname, 'publiic', 'index.html'), (err, data) => {
+            if (err) {
+                throw err
+            }
+            res.writeHead(200, {
+                "Content-type": "text/html"
+            })
+            res.end(data)
+        })
+    } else if (req.url === "/contact") {
+        fs.readFile(path.join(__dirname, 'publiic', 'contact.html'), (err, data) => {
+            if (err) {
+                throw err
+            }
+            res.writeHead(200, {
+                "Content-type": "text/html"
+            })
+            res.end(data)
+        })
+    }
+
 
 server.listen(3000, ()=>{
     console.log('server has been started...')
