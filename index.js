96d1431ca06f79c2be2efad63e832353aa72eb5f
qairkhan <shyngys94@gmail.com>
2020-06-07 17:24:23 +0300

    foo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http')
 const fs = require('fs')
 const path = require('path')
 
-const server = http.createServer((req, res)=> {
+// const server = http.createServer((req, res)=> {
 //     if (req.url === "/") {
 //         fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
 //             if (err) {
@@ -26,34 +26,39 @@ const server = http.createServer((req, res)=> {
 //     }
 // })
 
-let filePath = path.join(__dirname, "public", req.url === '/' ? 'index.html' : req.url)
-const ext = path.extname(filePath)
-if (!ext) {
-    filePath += '.html'
+// let filePath = path.join(__dirname, "public", req.url === '/' ? 'index.html' : req.url)
+// const ext = path.extname(filePath)
+// if (!ext) {
+//     filePath += '.html'
+// }
+// fs.readFile(filePath, (err, contents) => {
+//     if (err) {
+//         fs.readFile(path.join(__dirname, 'public', 'error.html'), (err, data) => {
+//             if(err) {
+//                 res.writeHead(500)
+//                 res.end('Error')
+//             } else {
+//                 res.writeHead(200, {
+//                     'Content-type' : 'text/html'
+//                 })
+//                 res.end(data)
+//             }
+//         })
+//     } else {
+//         res.writeHead(200, {
+//             'Content-type' : 'text/html'
+//         })
+//         res.end(contents)
+//     }
+// })
+// })
+
+
+// server.listen(3000, ()=>{
+//     console.log('server has been started...')
+// })
+
+function test () {
+    console.log('Hi');
 }
-fs.readFile(filePath, (err, contents) => {
-    if (err) {
-        fs.readFile(path.join(__dirname, 'public', 'error.html'), (err, data) => {
-            if(err) {
-                res.writeHead(500)
-                res.end('Error')
-            } else {
-                res.writeHead(200, {
-                    'Content-type' : 'text/html'
-                })
-                res.end(data)
-            }
-        })
-    } else {
-        res.writeHead(200, {
-            'Content-type' : 'text/html'
-        })
-        res.end(contents)
-    }
-})
-})
-
-
-server.listen(3000, ()=>{
-    console.log('server has been started...')
-})
+test();
