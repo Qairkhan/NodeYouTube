5b26d0f8b436ff37a47fbda6194176132ce188d5
Qairkhan <shyngys94@gmail.com>
2020-06-14 16:39:18 +0300

    fix

--- index.js
+++ index.js
@@ -99,8 +99,8 @@ app.get('/', function(req, res){
 app.get('/news', function(req, res){
     res.send('This is news')
 });
-app.get('/news/:id', function(req, res){
-    res.send('ID is - ' + req.params.id)
+app.get('/news/:name/:id', function(req, res){
+    res.send('ID is - ' + req.params.name + req.params.id)
 });
 
 app.listen(3000);
