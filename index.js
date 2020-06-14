0b326ae3d7c284980a40bcd5908dde96a22d5de7
Qairkhan <shyngys94@gmail.com>
2020-06-14 16:33:44 +0300

    fix

--- index.js
+++ index.js
@@ -95,6 +95,9 @@ const app = express();
 
 app.get('/', function(req, res){
     res.send('This is home')
-})
+});
+app.get('/news', function(req, res){
+    res.send('This is news')
+});
 
 app.listen(3000);
