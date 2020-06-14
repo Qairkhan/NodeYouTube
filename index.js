e9e6b273e0fc75c567e28ecd1b47b399885f635b
Qairkhan <shyngys94@gmail.com>
2020-06-14 16:36:55 +0300

    more links

--- index.js
+++ index.js
@@ -99,5 +99,8 @@ app.get('/', function(req, res){
 app.get('/news', function(req, res){
     res.send('This is news')
 });
+app.get('/news/:id', function(req, res){
+    res.send('ID is - ' + req.params.id)
+});
 
 app.listen(3000);
